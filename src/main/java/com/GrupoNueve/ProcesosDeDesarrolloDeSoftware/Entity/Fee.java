036a694e3657850f20fda85ce1cae9a9f0f88791
Valentín Campestri <157440254+VCampestriDev@users.noreply.github.com>
2024-04-20 14:24:54 -0300

    Update Initial

--- src/main/java/com/GrupoNueve/ProcesosDeDesarrolloDeSoftware/Entity/Fee.java
+++ src/main/java/com/GrupoNueve/ProcesosDeDesarrolloDeSoftware/Entity/Fee.java
@@ -1,4 +1,21 @@
 package com.GrupoNueve.ProcesosDeDesarrolloDeSoftware.Entity;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
 public class Fee {
+    Integer feeNumber;
+    List<Course> courses;
+    Student student;
+
+    public void addCourse(Course course) {
+        courses.add(course);
+    }
 }
--- src/main/java/com/GrupoNueve/ProcesosDeDesarrolloDeSoftware/Entity/Inscription.java
+++ src/main/java/com/GrupoNueve/ProcesosDeDesarrolloDeSoftware/Entity/Inscription.java
@@ -1,4 +1,12 @@
 package com.GrupoNueve.ProcesosDeDesarrolloDeSoftware.Entity;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
 public class Inscription {
 }
