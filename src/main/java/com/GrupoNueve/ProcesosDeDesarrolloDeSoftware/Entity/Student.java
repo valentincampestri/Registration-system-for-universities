05c17b36bd5662686a4dc726a7ab55bdebf419ac
pepi <fedeabeledo01@gmail.com>
2024-04-21 17:59:10 -0300

    implement SubjectService

--- src/main/java/com/GrupoNueve/ProcesosDeDesarrolloDeSoftware/Entity/Student.java
+++ src/main/java/com/GrupoNueve/ProcesosDeDesarrolloDeSoftware/Entity/Student.java
@@ -1,6 +1,6 @@
 package com.GrupoNueve.ProcesosDeDesarrolloDeSoftware.Entity;
 
-public class Student extends Person{
+public class Student extends Person {
 
     public Student(String name, String lastName, String personID, String email, String phone, String address) {
         super(name, lastName, personID, email, phone, address);
--- src/main/java/com/GrupoNueve/ProcesosDeDesarrolloDeSoftware/Service/Implementation/SubjectService.java
+++ src/main/java/com/GrupoNueve/ProcesosDeDesarrolloDeSoftware/Service/Implementation/SubjectService.java
@@ -1,4 +1,32 @@
 package com.GrupoNueve.ProcesosDeDesarrolloDeSoftware.Service.Implementation;
+import com.GrupoNueve.ProcesosDeDesarrolloDeSoftware.Entity.Subject;
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
 
 public class SubjectService {
+    Graph<Subject, DefaultEdge> subjectGraph;
+
+    public SubjectService() {
+        subjectGraph = new DefaultDirectedGraph<>(DefaultEdge.class);
+    }
+
+    public void addSubject(Subject subject) {
+        subjectGraph.addVertex(subject);
+    }
+
+    public void addPrerequisite(Subject subject, Subject prerequisite) {
+        subjectGraph.addEdge(subject, prerequisite);
+    }
+
+    public List<Subject> getPrerequisites(Subject subject) {
+        return Graphs.successorListOf(subjectGraph, subject);
+    }
 }
