7f999e134c8b740f4c6743ff3badafab402a6137
Valentín Campestri <157440254+VCampestriDev@users.noreply.github.com>
2024-04-28 16:57:49 -0300

    Prepared Test to complete

---
+++ src/test/java/com/GrupoNueve/ProcesosDeDesarrolloDeSoftware/service/ProfessorServiceTest.java
@@ -0,0 +1,49 @@
+package com.GrupoNueve.ProcesosDeDesarrolloDeSoftware.service;
+
+import com.GrupoNueve.ProcesosDeDesarrolloDeSoftware.Dto.Request.CourseRequestDto;
+import com.GrupoNueve.ProcesosDeDesarrolloDeSoftware.Exception.NotFoundException;
+import com.GrupoNueve.ProcesosDeDesarrolloDeSoftware.Repository.Implementation.*;
+import com.GrupoNueve.ProcesosDeDesarrolloDeSoftware.Service.Implementation.CourseService;
+import com.GrupoNueve.ProcesosDeDesarrolloDeSoftware.Service.Implementation.FeeService;
+import com.GrupoNueve.ProcesosDeDesarrolloDeSoftware.Service.Implementation.InscriptionService;
+import com.GrupoNueve.ProcesosDeDesarrolloDeSoftware.Service.Implementation.ProfessorService;
+import com.GrupoNueve.ProcesosDeDesarrolloDeSoftware.utils.MockBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@DirtiesContext
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class ProfessorServiceTest {
+    @Mock
+    ProfessorRepository professorRepository;
+    @InjectMocks
+    ProfessorService professorService;
+
+    @Test
+    @DisplayName("create - Error")
+    public void createFail() {
+        // Arrange
+        // Act & Assert
+    }
+
+    @Test
+    @DisplayName("create - Ok")
+    public void createOk() {
+        // Arrange
+        // Act
+        // Assert
+    }
+}
